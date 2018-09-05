d0c2f5cbbcc23efa6c7a1d5e2758b37eedf14e65
Nick O'Brien Cannon <obrienup@bc.edu>
2018-09-05 10:20:20 -0400

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Nicholas O'Brien Cannon on 9/5/18.
+//  Copyright © 2018 Nicholas O'Brien Cannon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
