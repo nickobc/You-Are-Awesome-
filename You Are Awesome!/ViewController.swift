b1d6906abe1e7403bb686cc72ba670fd39febafe
Nick O'Brien Cannon <obrienup@bc.edu>
2018-09-10 11:41:38 -0400

    2.13

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -33,7 +33,17 @@ class ViewController: UIViewController {
                     "You've got the design skills of Jony Ive!",
                     "I can't wait to download your app!"]
     
-    messageLabel.text = messages.randomElement()!
+    var newIndex = -1
+    
+    repeat {
+        newIndex = Int.random(in: 0..<messages.count)
+    } while index == newIndex
+    
+    index = newIndex
+    messageLabel.text = messages[index]
+    
+    
+//    messageLabel.text = messages.randomElement()!
     
     
 //messageLabel.text = messages[index]
